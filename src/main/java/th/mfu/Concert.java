263d633d2e6ec1f5041b85a64101ac0141fea0d4
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2023-08-17 11:47:57 +0700

    update exercises

---
+++ src/main/java/th/mfu/Concert.java
@@ -0,0 +1,59 @@
+package th.mfu;
+
+import java.util.Date;
+
+public class Concert {
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String location;
+    private String description;
+
+    
+
+    public Concert() {
+    }
+    public Concert(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+}
